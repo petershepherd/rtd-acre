835ff084dc4d0dc7cda57f5f482308f074d85815
petershepherd <49288445+petershepherd@users.noreply.github.com>
2026-01-19 19:13:04 +0100

    Add initial conf.py for Sphinx documentation
    Initialize Sphinx documentation configuration for ACRE project.

---
+++ docs/source/conf.py
@@ -0,0 +1,17 @@
+# -- Project information -----------------------------------------------------
+
+project = 'ACRE'
+author = 'ACRE Protocol'
+copyright = '2026, ACRE'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
